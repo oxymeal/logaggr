7f4b60fbc1393fae6a5b328b123834fb2e733cc9
Oleg Yamnikov <bgeyts667@gmail.com>
2019-07-03 20:27:47 +0500

    Add TestAppendLogLine
    Co-authored-by: Alexandr Vagner <alexmednogorsk@mail.ru>

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -1,7 +1,9 @@
 package storage
 
 import (
+	"encoding/json"
 	"io"
+	"io/ioutil"
 	"strings"
 	"testing"
 
@@ -57,4 +59,38 @@ func TestReadLogLines(t *testing.T) {
 		"d": []interface{}{1.0, 2.0, 3.0},
 		"e": map[string]interface{}{"ee": 1.0},
 	}, lines[1])
+}
+
+func TestAppendLogLine(t *testing.T) {
+	require := require.New(t)
+
+	// Create a temporary file and store its `path`
+	file, err := ioutil.TempFile("", "TestAppendLogLine.txt")
+	require.Nil(err)
+
+	path := file.Name()
+	err = file.Close()
+	require.Nil(err)
+
+	// Call appendLogLine
+	err = appendLogLine(path, LogLine{
+		"a": 1.0,
+		"b": "hello world",
+		"c": true,
+	})
+	require.Nil(err)
+
+	// Read the file again
+	content, err := ioutil.ReadFile(path)
+	require.Nil(err)
+
+	// Assert that its content equals appendLogLine argument
+	var logLine LogLine
+	err = json.Unmarshal(content, &logLine)
+	require.Nil(err)
+	require.Equal(LogLine{
+		"a": 1.0,
+		"b": "hello world",
+		"c": true,
+	}, logLine)
 }
